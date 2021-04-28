d50f092bad0aa3af4e1bc82ca512bb9ce858e867
Ashok Mane <ashokmane81800@gmail.com>
2021-04-28 20:49:08 +0530

    [ashok] added a file to insert at first linked list

--- src/main/java/com/bridgelabz/LinkedList.java
+++ src/main/java/com/bridgelabz/LinkedList.java
@@ -1,32 +1,66 @@
 package com.bridgelabz;
 
 public class LinkedList<K> {
-    private  LinkedList next;
-    private K key;
+  int size;
+  Node start;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Linked List Operations");
     }
     // ability to create linked list 56,30 and 70
 
-    public LinkedList(K key) {
-        this.key = key;
-        this.next = null;
+
+    public LinkedList( ) {
+        this.size = 0;
+        this.start = null;
     }
 
-    public LinkedList getNext() {
-        return next;
+    public int getSize() {
+        return size;
     }
 
-    public void setNext(LinkedList next) {
-        this.next = next;
+
+    /**
+     * insert node at beigining 30 qnd 56 to 70
+     * @return
+     */
+//    public static<K extends Comparable<K>> void inserBegining(int val) {
+//        Node n;
+//        n = new Node();
+//        n.getData(val);
+//        n.setNext(start);
+//        start = n;
+//        size++;
+//    }
+
+    public boolean isEmpty() {
+      if(start == null){
+        return true;
+    }
+      else
+          return  false;
     }
 
-    public K getKey() {
-        return key;
+    public void viewlist() {
+    Node t;
+    if(isEmpty()) {
+        System.out.println("list is empty");
+    }
+    else {
+        t = start;
+        for(int i = 0;i<size;i++) {
+            System.out.print(t.getData()+"  ->  ");
+            t = t.getNext();
+            }
+        }
     }
 
-    public void setKey(K key) {
-        this.key = key;
+    public void insertFirst(int val) {
+        Node n;
+        n = new Node();
+        n.setData(val);
+        n.setNext(start);
+        start = n;
+        size++;
     }
 }
--- src/test/java/com/bridgelabz/LinkedListTest.java
+++ src/test/java/com/bridgelabz/LinkedListTest.java
@@ -3,19 +3,15 @@ package com.bridgelabz;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class LinkedListTest {
     @Test
-    public void givenValuesToCreateLinkedList() {
-        LinkedList linkedList = new LinkedList(56);
-        LinkedList linkedList1 = new LinkedList(30);
-        LinkedList linkedList2 = new LinkedList(70);
-
-        linkedList.setNext(linkedList1);
-        linkedList1.setNext(linkedList2);
-
-        boolean result = linkedList.getNext().equals(linkedList1) &&
-                linkedList1.getNext().equals(linkedList2);
-
-        Assert.assertTrue(result);
+    public void createLinkedlistAddOneByOneToFront() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.insertFirst(70);
+        linkedList.insertFirst(30);
+        linkedList.insertFirst(56);
+          linkedList.viewlist();
     }
 }
