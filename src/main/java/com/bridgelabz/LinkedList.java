1890bad71ca2b427f28cdb2c1cfdbbbebb6f5dfc
Ashok Mane <ashokmane81800@gmail.com>
2021-04-28 12:17:34 +0530

    [ashok] created a linked list with some nodes

--- src/main/java/com/bridgelabz/LinkedList.java
+++ src/main/java/com/bridgelabz/LinkedList.java
@@ -1,7 +1,32 @@
 package com.bridgelabz;
 
 public class LinkedList<K> {
+    private  LinkedList next;
+    private K key;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Linked List Operations");
+    }
+    // ability to create linked list 56,30 and 70
+
+    public LinkedList(K key) {
+        this.key = key;
+        this.next = null;
+    }
+
+    public LinkedList getNext() {
+        return next;
+    }
+
+    public void setNext(LinkedList next) {
+        this.next = next;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
     }
 }
--- src/test/java/com/bridgelabz/LinkedListTest.java
+++ src/test/java/com/bridgelabz/LinkedListTest.java
@@ -1,4 +1,21 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class LinkedListTest {
+    @Test
+    public void givenValuesToCreateLinkedList() {
+        LinkedList linkedList = new LinkedList(56);
+        LinkedList linkedList1 = new LinkedList(30);
+        LinkedList linkedList2 = new LinkedList(70);
+
+        linkedList.setNext(linkedList1);
+        linkedList1.setNext(linkedList2);
+
+        boolean result = linkedList.getNext().equals(linkedList1) &&
+                linkedList1.getNext().equals(linkedList2);
+
+        Assert.assertTrue(result);
+    }
 }
